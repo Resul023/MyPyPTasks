8ffff5014a0ce2c8824ea258a1276db29baeb565
Resul023 <73m6u7h@code.edu.az>
2022-12-13 23:02:10 +0400

    Reflection Task

---
+++ 13.12.2022/Reflection/Reflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using Reflection;
+using System;
+using System.Reflection;
+
+class Program
+{
+    static public void Main(String[] args)
+    {
+        User newUser = new User("Yilmaz","yilmaz@code.edu.az");
+        GetAllProperties(newUser);
+        var myMethod = newUser.GetType().GetMethod("GetEmail");
+        string myEmail = "yilmazNewEmail@code.edu.az";
+        var myMethodInvoke = myMethod.Invoke(newUser, new object[] { myEmail });
+        Console.WriteLine(myMethodInvoke);
+    }
+    static void GetAllProperties(User user)
+    {
+        foreach (var item in user.GetType().GetProperties())
+        {
+            Console.WriteLine($"{item.Name}-{item.GetValue(user)}");
+        }
+    }
+}
---
+++ 13.12.2022/Reflection/Reflection/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class User
+    {
+        public User(string userName, string email)
+        {
+            UserName = userName;
+            Email = email;
+        }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public string GetEmail(string email)
+        {
+            return $"Email-{email}";
+        }
+
+    }
+}
