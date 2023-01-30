372564959f6da151a7fc8f67e9eecc46aeee056c
Resul023 <73m6u7h@code.edu.az>
2023-01-30 18:51:30 +0400

    FrontToBack

---
+++ FrontToBack/WebApplication1/WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+	public class BlogController : Controller
+	{
+		
+		public IActionResult Blog()
+		{
+			return View();
+		}
+		public IActionResult Details() 
+		{
+			return View();
+		}
+	}
+}
