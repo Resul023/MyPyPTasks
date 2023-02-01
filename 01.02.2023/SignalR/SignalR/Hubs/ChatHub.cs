6116f8a82996ebe2e76dbe829ea35d20f159dad1
Resul023 <73m6u7h@code.edu.az>
2023-02-01 22:16:21 +0400

    SignalR

---
+++ 01.02.2023/SignalR/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendToUser(string user, string message, string receiverConnectionId)
+            => await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage",user,message);
+
+        public string GetConnectionId() => Context.ConnectionId;
+    }
+}
---
+++ 01.02.2023/SignalR/SignalR/wwwroot/js/Chat.js
@@ -0,0 +1,29 @@
+﻿
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+connection.start().then(function () {
+    connection.invoke("GetConnectionId").then(function (id) {
+        document.getElementById("connectionId").innerText = id;
+    })
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendToUser").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    var receiverConnectionId = document.getElementById("receiverId").value;
+    connection.invoke("SendToUser",user,message,receiverConnectionId).catch(function (err) {
+        return console.error(err.toString());
+    });
+    document.getElementById("messageInput").value = "";
+    event.preventDefault();
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message;
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
